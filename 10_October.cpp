f9525e6fd674843778321d62f399ad9325284cf0
Manav Malhotra <mmalhotra_be21@thapar.edu>
2023-10-10 12:39:32 +0530

    New file 10 october 2023

---
+++ 10_October.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
