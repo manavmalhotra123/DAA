18e262c4b3dffbe60d0cb850177cb3bc2409d18f
Manav Malhotra <mmalhotra_be21@thapar.edu>
2023-12-01 13:23:37 +0530

    done

--- DynamicProgramming/CoinChanging.cpp
+++ DynamicProgramming/CoinChanging.cpp
@@ -4,29 +4,6 @@
 
 using namespace std;
 
-// Recursive approach
-int RecursiveMinCoinChange(int target, vector<int>& coins)
-{
-    if (target == 0) {
-        return 0; // Base case: 0 coins needed for 0 amount
-    }
-
-    int result = INT_MAX;
-    for (int i = 0; i < coins.size(); ++i)
-    {
-        if (coins[i] <= target)
-        {
-            int subResult = RecursiveMinCoinChange(target - coins[i], coins);
-            if (subResult != INT_MAX && subResult + 1 < result)
-            {
-                result = subResult + 1;
-            }
-        }
-    }
-
-    return result;
-}
-
 // Dynamic Programming approach
 int MinCoinChange(int target, vector<int>& coins)
 {
@@ -55,7 +32,6 @@ int main()
     int minCoinsRecursive = RecursiveMinCoinChange(target, coins);
     int minCoinsDP = MinCoinChange(target, coins);
 
-    cout << "Recursive approach - Minimum number of coins needed: " << minCoinsRecursive << endl;
     cout << "Dynamic Programming - Minimum number of coins needed: " << minCoinsDP << endl;
 
     return 0;
